ed693cc34f9771d9c474d0f8c51e154a2e909c5f
longqi <longqi90@gmail.com>
2016-10-07 17:11:05 +0800

    init2

--- app/course.service.ts
+++ app/course.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CourseService {
+	socket;
 	getCourses(): string[] {
 		return ['course 1', 'course 2', 'course 3'];
+	}
+	constructor(){
+		this.socket = new WebSocket('ws://localhost:8888/bic/con');
+		this.socket.onmessage()
 	}
 }
