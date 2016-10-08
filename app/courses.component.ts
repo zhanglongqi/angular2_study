edb4db26d0e4afc035945adc9474eb13e4ca7579
longqi <longqi90@gmail.com>
2016-10-08 14:17:25 +0800

    first try of websocket

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -4,6 +4,7 @@ import {CourseService} from './course.service'
 @Component({
 	selector: "my-courses",
 	template: `<h2>{{title}}</h2>
+						{{author}}
 						<ul>
 						<li *ngFor="let course of courses">
 						<strong>{{ course }}</strong>
@@ -15,9 +16,19 @@ import {CourseService} from './course.service'
 export class CoursesComponent {
 	title = 'Title of Course';
 	courses = [];
+	author: string;
+	socket;
 
 	constructor(private courseService: CourseService) {
 
 		this.courses = this.courseService.getCourses();
+
+		this.socket = new WebSocket('ws://localhost:8888/bic/con');
+		this.socket.onmessage = function (ev) {
+			var data = JSON.parse(ev.data);
+			this.author = data['name'];
+			console.info(data);
+		}
 	}
+
 }
